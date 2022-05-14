630fda464172708efbf195de8a33a6cda1651bf0
emre <emre@marketyo.com>
2022-05-14 18:43:01 +0300

    Created Colors asset for textColor

---
+++ SpaceGame/Base/Constans.swift
@@ -0,0 +1,35 @@
+//
+//  Constans.swift
+//  SpaceGame
+//
+//  Created by Emre on 14.05.2022.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    
+}
+
+
+struct Colors {
+    public static var backgroundColor: UIColor {
+        get {
+            if #available(iOS 13.0, *) {
+                return UIColor.systemBackground
+            } else {
+                return .white
+            }
+        }
+    }
+    public static var textColor: UIColor {
+        get {
+            if #available(iOS 13.0, *) {
+                return UIColor(named: "TextColor") ?? .black
+            } else {
+                return .black
+            }
+        }
+    }
+}
--- SpaceGame/ViewControllers/SpaceShipVC.swift
+++ SpaceGame/ViewControllers/SpaceShipVC.swift
@@ -9,10 +9,32 @@ import UIKit
 
 class SpaceShipVC: BaseVC {
     
-    private let pointsLabel: UILabel = {
+    private let pointsTitleLabel: UILabel = {
         let lbl = UILabel()
         lbl.text = "Dağıtılacak Puan"
+        lbl.font = UIFont.boldSystemFont(ofSize: 20)
         return lbl
+    }()
+    
+    private let pointsLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.text = "12"
+        lbl.font = UIFont.boldSystemFont(ofSize: 30)
+        lbl.textAlignment = .center
+        return lbl
+    }()
+    
+    private let pointsHolderView: UIView = {
+        let view = UIView()
+        view.layer.borderWidth = 1.5
+        view.layer.borderColor = Colors.textColor.cgColor
+        return view
+    }()
+    
+    private let dividerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = Colors.textColor
+        return view
     }()
 
     override func viewDidLoad() {
@@ -22,14 +44,34 @@ class SpaceShipVC: BaseVC {
     }
     
     override func setupSubviews() {
-        [pointsLabel].forEach{view.addSubview($0)}
+        [pointsTitleLabel, pointsHolderView, dividerView].forEach{view.addSubview($0)}
+        [pointsLabel].forEach{pointsHolderView.addSubview($0)}
     }
     
     override func setupLayouts() {
-        pointsLabel.snp.makeConstraints { make in
+        pointsHolderView.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(statusBarHeight() + 20)
+            make.trailing.equalToSuperview().offset(-20)
+            make.height.equalTo(50)
+            make.width.equalTo(60)
+        }
+        
+        pointsTitleLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(pointsHolderView)
             make.leading.equalToSuperview().offset(20)
         }
+        
+        pointsLabel.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        dividerView.snp.makeConstraints { make in
+            make.top.equalTo(pointsHolderView.snp.bottom).offset(20)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+            make.height.equalTo(2)
+        }
+        
     }
 
 }
